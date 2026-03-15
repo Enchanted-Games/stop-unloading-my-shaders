a699919c4853d3e59f9b41a58f91eb292206d5f9
ioblackshaw <isaacoblackshaw@gmail.com>
2026-03-15 18:57:58 +0000

    make the shader reload key rebindable

---
+++ src/main/java/games/enchanted/eg_stop_unloading_my_shaders/common/SUMRMod.java
@@ -0,0 +1,30 @@
+package games.enchanted.eg_stop_unloading_my_shaders.common;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+
+//? if fabric {
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+//? }
+
+public class SUMRMod {
+    public static final KeyMapping HOT_RELOAD_KEYBIND = new KeyMapping(
+        "key.eg_stop_unloading_my_shaders.reload_shaders",
+        InputConstants.Type.KEYSYM,
+        InputConstants.KEY_R,
+        KeyMapping.Category.DEBUG
+    );
+
+    public static void init() {
+        Logging.info("Mod init started. Compiled for {}", ModConstants.TARGET_PLATFORM);
+        //? if fabric {
+        registerKeybinds();
+        //? }
+    }
+
+    public static void registerKeybinds() {
+        //? if fabric {
+        KeyMappingHelper.registerKeyMapping(HOT_RELOAD_KEYBIND);
+        //? }
+    }
+}
--- src/main/java/games/enchanted/eg_stop_unloading_my_shaders/common/mixin/KeyboardHandlerMixin.java
+++ src/main/java/games/enchanted/eg_stop_unloading_my_shaders/common/mixin/KeyboardHandlerMixin.java
@@ -1,9 +1,11 @@
 package games.enchanted.eg_stop_unloading_my_shaders.common.mixin;
 
+import games.enchanted.eg_stop_unloading_my_shaders.common.SUMRMod;
 import games.enchanted.eg_stop_unloading_my_shaders.common.ShaderReloadManager;
 import net.minecraft.client.KeyboardHandler;
 import net.minecraft.client.input.KeyEvent;
 import org.lwjgl.glfw.GLFW;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,12 +14,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(KeyboardHandler.class)
 public abstract class KeyboardHandlerMixin {
     @Inject(
-        at = @At("RETURN"),
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/client/Minecraft;options:Lnet/minecraft/client/Options;",
+            opcode = Opcodes.GETFIELD,
+            ordinal = 0
+        ),
         method = "handleDebugKeys",
         cancellable = true
     )
     private void eg_sumr$addReloadShadersKey(KeyEvent keyEvent, CallbackInfoReturnable<Boolean> cir) {
-        if(keyEvent.key() == GLFW.GLFW_KEY_R) {
+        if(SUMRMod.HOT_RELOAD_KEYBIND.matches(keyEvent)) {
             ShaderReloadManager.triggerReload();
             cir.setReturnValue(true);
         }
